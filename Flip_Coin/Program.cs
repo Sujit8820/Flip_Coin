685aa1f2c1542439c26e80d6af1bb67ad8253ed7
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-09 17:20:59 +0530

    ADDED: Multiple times of flipping a coin and show no of times head & tail has won

--- Flip_Coin/Program.cs
+++ Flip_Coin/Program.cs
@@ -8,22 +8,31 @@ namespace Flip_Coin
 
         static void Main(string[] args)
         {
-          
-            Random random = new Random();
-            int flip = random.Next(0, 2);
-
-            int HEAD = 0;
-            //int TAIL = 1;
-
-            if (flip == HEAD)
+            int no_of_flip = 0;
+            int head = 0;
+            int tail = 0;
+            while (no_of_flip < 17)
             {
-                Console.WriteLine("Head");
-            }
-            else
-            {
-                Console.WriteLine("Tail");
-            }
 
+                Random random = new Random();
+                int flip = random.Next(0, 2);
+
+                int HEAD = 0;
+                //int TAIL = 1;
+
+                if (flip == HEAD)
+                {
+                    head = head + 1;   
+                    //Console.WriteLine("Head");
+                }
+                else
+                {
+                    tail = tail + 1;
+                   // Console.WriteLine("Tail");
+                }
+                no_of_flip++;
+            }
+            Console.WriteLine($"Head:{head} & Tail:{tail}");
         }
 
     }
