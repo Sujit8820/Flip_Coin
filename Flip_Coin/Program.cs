41d091966c382e8d095d6ec7ae7e2749df8b12b4
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-09 17:04:40 +0530

    ADDED: Start with flipping a coin to display winner

--- Flip_Coin/Program.cs
+++ Flip_Coin/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Flip_Coin
+{ 
+    class Programe
+    {
+
+        static void Main(string[] args)
+        {
+          
+            Random random = new Random();
+            int flip = random.Next(0, 2);
+
+            int HEAD = 0;
+            //int TAIL = 1;
+
+            if (flip == HEAD)
+            {
+                Console.WriteLine("Head");
+            }
+            else
+            {
+                Console.WriteLine("Tail");
+            }
+
+        }
+
+    }
+
+}
